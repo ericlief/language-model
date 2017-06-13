c5aed44c6d5f356e25be36a91274894a3d87cb35
Eric Lief <liefe@bossa>
2017-06-13 11:46:54 +0200

    wap mod

--- wordassociationpairs.py
+++ wordassociationpairs.py
@@ -51,13 +51,15 @@ class WordAssociationPairs:
                     continue
                 pmi = np.log2(count * self._T / (c_w1 * c_w2))
                 self.pmi_adj_pairs[w1][w2] = pmi
+        print('done with adj pairs')
 
     def count_distant_word_pairs(self):
+        print('getting dist counts')
         self.counts_dist_pairs = defaultdict(lambda: defaultdict(lambda: 0))
         for i, w1 in enumerate(self._tokens):
             # Get pairs to the right
             dist = 0
-            for j, w2 in enumerate(self._tokens[i:]):     # skip adj word?
+            for w2 in self._tokens[i:]:     # skip adj word?
                 if 1 < dist <= 50:
                     self.counts_dist_pairs[w1][w2] += 1
                     # print('dist ', dist)
@@ -68,17 +70,19 @@ class WordAssociationPairs:
             # tokens = self._tokens[]
             dist = 0
             # print(self._tokens[i-2::-1])
-            for j, w2 in enumerate(self._tokens[i::-1]):
+            for w2 in self._tokens[i::-1]:
                 if 1 < dist <= 50:
                     self.counts_dist_pairs[w1][w2] += 1
                     # print('dist ', dist)
                     # print(w1, w2)
                 dist += 1
                 # print(i, w1, j, w2)
-        print(self.counts_dist_pairs)
+        #print(self.counts_dist_pairs)
+        print('done w counts')
 
     def pointwise_mutual_information_dist_pairs(self):
         self.pmi_dist_pairs = defaultdict(lambda: defaultdict(lambda: 0.0))
+        print('getting pmi ')
         for w1, seconds in self.counts_dist_pairs.items():
             for w2, count in seconds.items():
                 c_w1 = self._word_counts[w1]
@@ -86,7 +90,8 @@ class WordAssociationPairs:
                 if c_w1 + c_w2 < 20:
                     continue
                 pmi = np.log2(count * self._T / (c_w1 * c_w2))
-                self.pmi_adj_pairs[w1][w2] = pmi
+                self.pmi_dist_pairs[w1][w2] = pmi
+                print(w1,w2,pmi)
 
     @property
     def T(self):
